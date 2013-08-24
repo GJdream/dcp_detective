42b185afdb5388820e55b974bbd3015fefe10757
Jeff Schwab <jeff@schwabcenter.com>
2013-08-24 16:55:35 -0400

    Add simple domain types.

--- app/DCPDetective/DCPDetective/ZJAppDelegate.m
+++ app/DCPDetective/DCPDetective/ZJAppDelegate.m
@@ -8,11 +8,53 @@
 
 #import "ZJAppDelegate.h"
 
+@interface Chunk : NSObject
+@property NSString *path;
+@property int       volumeIndex;
+@end
+
+@interface Asset : NSObject
+@property NSString *assetID;
+@property NSArray  *chunkList;
+@end
+
+@interface AssetMap : NSObject
+@property NSArray *assetList;
+// TODO: Add remaining AssetMap properties.
+@end
+
+static NSArray *loadAssetMap(NSString *dir)
+    // TODO: Contract
+{
+    // If dir/ASSETMAP.xml exists,
+    //  parse it as SMPTE;
+    // else, if ASSETMAP exists,
+    //  parse it as Interop.
+
+    // TODO: Process each subdirectory of dir not called lost+found or RECYCLER.
+
+    return nil;
+}
+
 @implementation ZJAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    // For each command-line argument (except the first, which is just the name
+    // of this executable), find the name of the PKL file.
+
+    NSMutableArray *args =
+    [[[NSProcessInfo processInfo] arguments] mutableCopy];
+    [args removeObjectAtIndex:0];
+
+    for (NSString *arg in args) {
+        NSLog(@"Detecting DCP: %@", arg);
+    }
 }
 
 @end
+
+@implementation Chunk
+@end
+
+// TODO: Add implementations of remaining interfaces.
