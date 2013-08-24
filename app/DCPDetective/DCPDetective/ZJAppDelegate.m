0eadb1a0d7ee3844843841f0cba67129adc8da41
Zachary Manning <zachary.manning@gmail.com>
2013-08-24 19:14:16 -0400

    Refactor slightly.

--- app/DCPDetective/DCPDetective/ZJAppDelegate.m
+++ app/DCPDetective/DCPDetective/ZJAppDelegate.m
@@ -46,34 +46,48 @@ NSXMLDocument *loadXMLFile(NSString *fileName)
 }
 
 ZJAssetMap *parseSMPTEAssetMap(NSString *fileName)
-    // Parse the file having the specified fileName as an SMPTE asset map.
+    // Parses the file having the specified fileName as an SMPTE asset map.
 {
     return nil; // TODO
 }
 
+NSString *getId(NSXMLElement *elem)
+    // Returns the text of the 'Id' sub-element of 'elem', or nil if 'elem'
+    // does not have exactly one 'Id' element.
+{
+    NSArray *children = [elem elementsForName:@"Id"];
+
+    if ([children count] != 1) {
+        NSLog(@"Warning: expected 1 Id element in node");
+        return nil;
+    }
+
+    return [[children objectAtIndex:0] stringValue];
+}
+
 ZJAssetMap *parseInteropAssetMap(NSString *fileName)
-    // Parse the file having the specified fileName as an Interop-formatted
+    // Parses the file having the specified fileName as an Interop-formatted
     // asset map.
 {
+    ZJAssetMap *map = [[ZJAssetMap alloc] init];
     NSXMLDocument *doc = loadXMLFile(fileName);
-    
+
     if (!doc) {
         return nil;
     }
-    
+
     NSXMLElement *root = [doc rootElement];
-    
-    NSLog(@"%@", root);
-    
-    ZJAssetMap *map = [[ZJAssetMap alloc] init];
-    
-//    map.assetMapID =
-    
+
+    map.assetMapID = getId(root);
+
     return map; // TODO
 }
 
 static ZJAssetMap *loadAssetMap(NSString *dir)
-    // TODO: Contract
+    // Returns an object representing the asset map in directory 'dir', or nil
+    // if no asset map exists, or the asset map cannot be parsed.  Parses the
+    // asset map as SMPTE format if called ASSETMAP.xml, or in Interop format if
+    // called ASSETMAP (with no extension).
 {
     // If dir/ASSETMAP.xml exists,
     //  parse it as SMPTE;
@@ -114,8 +128,7 @@ static ZJAssetMap *loadAssetMap(NSString *dir)
     for (NSString *arg in args) {
         //NSLog(@"Detecting DCP: %@", arg);
         ZJAssetMap *assetMap = loadAssetMap(arg);
-        (void)assetMap;
-
+        NSLog(@"%@", assetMap.assetMapID);
     }
 }
 
