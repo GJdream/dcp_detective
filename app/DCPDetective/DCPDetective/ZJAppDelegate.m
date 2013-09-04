50dd7e4699eedae81bcf6ac3b3622f12dabe9c6b
Jeff Schwab <jeff@schwabcenter.com>
2013-09-04 11:31:06 -0400

    Report missing asset map file.

--- app/DCPDetective/DCPDetective/ZJAppDelegate.m
+++ app/DCPDetective/DCPDetective/ZJAppDelegate.m
@@ -6,12 +6,40 @@
 //  Copyright (c) 2013 Unbuggy Software LLC. All rights reserved.
 //
 
-#import "ZJAsset.h"
-#import "ZJAssetMap.h"
-#import "ZJChunk.h"
-#import "ZJFileUtil.h"
-
 #import "ZJAppDelegate.h"
+
+#import "ZJFileUtil.h"
+#import "ZJXMLRichElement.h"
+
+enum ZJAssetMapFormat {
+    ZJAssetMapFormatSMPTE,
+    ZJAssetMapFormatInterop
+};
+
+static ZJXMLRichElement *assetMapFromDCP(NSString              *path,
+                                         enum ZJAssetMapFormat *format)
+    // Returns the root element of the asset map in the specified directory
+    // 'path', and sets '*format' to indicate the asset map format: SMPTE if the
+    // file is called ASSETMAP.xml, or Interop if the file is called ASSETMAP.
+    // Returns nil, and leaves '*format' in an unspecified state, if the asset
+    // map cannot be parsed.
+{
+    NSString *smpteName = [path stringByAppendingPathComponent:@"ASSETMAP.xml"];
+
+    if ([ZJFileUtil fileExists:smpteName]) {
+        *format = ZJAssetMapFormatSMPTE;
+        return [ZJXMLRichElement elementWithContentsOfFile:smpteName];
+    }
+
+    NSString *interopName = [path stringByAppendingPathComponent:@"ASSETMAP"];
+
+    if ([ZJFileUtil fileExists:interopName]) {
+        *format = ZJAssetMapFormatInterop;
+        return [ZJXMLRichElement elementWithContentsOfFile:interopName];
+    }
+
+    return nil;
+}
 
 static NSString *dcpOK(NSString *path)
     // Considers the DCP directory at 'path'; returns a descriptive message if
@@ -20,7 +48,18 @@ static NSString *dcpOK(NSString *path)
     //
     // * An ASSETMAP file is found at the root of the DCP directory.
     // * All asset sizes and hashes match the associated packing list.
+    //
+    // The result message may contain multiple lines.  If so, the message is
+    // prefixed by a newline ('\n') and each line is prefixed with a tab ('\t')
+    // to appear indented one level when logged.
 {
+    enum ZJAssetMapFormat format;
+    ZJXMLRichElement *assetMap = assetMapFromDCP(path, &format);
+
+    if (!assetMap) {
+        return @"asset map could not be parsed";
+    }
+
     return nil;
 }
 
@@ -43,12 +82,14 @@ static NSString *dcpOK(NSString *path)
         NSString *message = dcpOK(path);
 
         if (message) {
-            puts("Inconsistencies:");
             puts([message UTF8String]);
         }
         else {
             puts("OK");
         }
+
+        // TODO: Process each subdirectory of dir not called lost+found or
+        // RECYCLER.
     }
 
     // Exit the application.
